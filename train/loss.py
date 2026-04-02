332040afb23736d2f15f8e08e3fefa6672ac43cd
Isam Balghari <isamabdullah88@gmail.com>
2026-04-02 21:52:29 +0500

    Exchange loss.

--- train/loss.py
+++ train/loss.py
@@ -1,15 +1,17 @@
 import torch.nn.functional as F
 
 class MultiTaskLoss:
-    def __init__(self, energy_weight=1.0, force_weight=1000.0):
-        self.we = energy_weight
-        self.wf = force_weight
+    def __init__(self, wenergy=1.0, wforce=100.0, wexchange=1.0):
+        self.we = wenergy
+        self.wf = wforce
+        self.wx = wexchange
 
-    def __call__(self, energy_pred, forces_pred, batch):
+    def __call__(self, epred, fpred, xpred, batch):
         """Calculates and weights the multi-task MSE loss."""
-        loss_e = F.mse_loss(energy_pred, batch.y_energy)
-        loss_f = F.mse_loss(forces_pred, batch.y_forces)
+        loss_e = F.mse_loss(epred, batch.y_energy)
+        loss_f = F.mse_loss(fpred, batch.y_forces)
+        loss_x = F.mse_loss(xpred, batch.y_exchange)
         
-        loss_tot = (self.we * loss_e) + (self.wf * loss_f)
+        loss_tot = (self.we * loss_e) + (self.wf * loss_f) + (self.wx * loss_x)
         
-        return loss_tot, loss_e, loss_f
+        return loss_tot, loss_e, loss_f, loss_x
