b1b0d32de503b2c6ee0224c0857f21c5087203f0
Isam Balghari <isamabdullah88@gmail.com>
2026-04-03 16:32:15 +0500

    Exchange loss weighted by bond distances.

--- train/loss.py
+++ train/loss.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn.functional as F
 
 class MultiTaskLoss:
@@ -6,12 +7,23 @@ class MultiTaskLoss:
         self.wf = wforce
         self.wx = wexchange
 
+    def exchangeloss(self, xpred, batch):
+        edgedists = batch.cr_edge_dist
+        jvals = batch.y_exchange
+
+        weights = edgedists / edgedists.mean()
+        
+        wlossx = torch.mean(weights.view(-1, 1) * F.l1_loss(xpred, jvals, reduction='none'))
+
+        return wlossx
+
+
     def __call__(self, epred, fpred, xpred, batch):
         """Calculates and weights the multi-task MSE loss."""
         loss_e = F.mse_loss(epred, batch.y_energy)
         loss_f = F.mse_loss(fpred, batch.y_forces)
         # loss_x = F.mse_loss(xpred, batch.y_exchange)
-        loss_x = F.l1_loss(xpred, batch.y_exchange)
+        loss_x = self.exchangeloss(xpred, batch)
         
         loss_tot = (self.we * loss_e) + (self.wf * loss_f) + (self.wx * loss_x)
         
