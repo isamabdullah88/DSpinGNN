8c26687e3d113e89f435b4364910b48989859109
Isam Balghari <isamabdullah88@gmail.com>
2026-04-03 15:55:10 +0500

    L1 loss for exchange.

--- train/loss.py
+++ train/loss.py
@@ -10,7 +10,8 @@ class MultiTaskLoss:
         """Calculates and weights the multi-task MSE loss."""
         loss_e = F.mse_loss(epred, batch.y_energy)
         loss_f = F.mse_loss(fpred, batch.y_forces)
-        loss_x = F.mse_loss(xpred, batch.y_exchange)
+        # loss_x = F.mse_loss(xpred, batch.y_exchange)
+        loss_x = F.l1_loss(xpred, batch.y_exchange)
         
         loss_tot = (self.we * loss_e) + (self.wf * loss_f) + (self.wx * loss_x)
         
