c121e0a18e21142b9806c9ac1fb8ae601ee1215a
Isam Balghari <isamabdullah88@gmail.com>
2026-04-05 01:25:12 +0500

    Fix in loss for exchange.

--- train/loss.py
+++ train/loss.py
@@ -20,11 +20,15 @@ class MultiTaskLoss:
 
     def __call__(self, epred, fpred, xpred, batch):
         """Calculates and weights the multi-task MSE loss."""
-        loss_e = F.mse_loss(epred, batch.y_energy)
-        loss_f = F.mse_loss(fpred, batch.y_forces)
+        # loss_e = F.mse_loss(epred, batch.y_energy)
+        # loss_f = F.mse_loss(fpred, batch.y_forces)
         # loss_x = F.mse_loss(xpred, batch.y_exchange)
-        loss_x = self.exchangeloss(xpred, batch)
+        loss_e = torch.tensor(0.0, device=epred.device)  # Placeholder for energy loss
+        loss_f = torch.tensor(0.0, device=fpred.device)  # Placeholder for force loss
+        # loss_x = self.exchangeloss(xpred, batch)
+        lossx = F.l1_loss(xpred, batch.y_exchange)
         
-        loss_tot = (self.we * loss_e) + (self.wf * loss_f) + (self.wx * loss_x)
+        # loss_tot = (self.we * loss_e) + (self.wf * loss_f) + (self.wx * loss_x)
+        loss_tot = lossx * self.wx
         
-        return loss_tot, loss_e, loss_f, loss_x
+        return loss_tot, loss_e, loss_f, lossx
