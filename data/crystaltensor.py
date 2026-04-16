78f6c0e4d27a7fe125cc431a5e5aecc2d5bb51a6
Isam Balghari <isamabdullah88@gmail.com>
2026-04-16 15:51:33 +0500

    Cleanup.

--- data/crystaltensor.py
+++ data/crystaltensor.py
@@ -22,38 +22,24 @@ class CrystalGraphTensor:
         
         mx, my = torch.meshgrid(torch.tensor([-1, 0, 1], dtype=torch.int64), torch.tensor([-1, 0, 1], dtype=torch.int64), indexing='ij')
         mz = torch.zeros_like(mx)
-        # print('mx: ', mx.dtype, mx.shape)
-        # print('my: ', my.dtype, my.shape)
-        # print('mz: ', mz.dtype, mz.shape)
 
         stack = torch.stack((mx.flatten(), my.flatten(), mz.flatten()), axis=1)
-        # print('stack: ', stack.dtype, stack.shape)
-        # print('stack: ', [tuple(row.tolist()) for row in stack])
         
         translations = torch.mm(stack.to(torch.float32), cell)
-        # print('\npositions: \n', [tuple(row.tolist()) for row in pos])
-        # print('\ntranslations: \n', [tuple(row.tolist()) for row in translations])
 
         ghostpos = pos.unsqueeze(0) + translations.unsqueeze(1)
         ghostpos = ghostpos.view(-1, 3)
-        # print('\nghostpos: \n', [tuple(row.tolist()) for row in ghostpos])
 
         dist = torch.cdist(ghostpos, pos)
-        # print('\ndist: ', dist.dtype, dist.shape)
 
         mask = (dist > 1e-4) & (dist <= rcut)
 
         ghostidxs, atomidxs = torch.where(mask)
-        # print('\nindex matrix: \n', [(i, j) for i, j in zip(ghostidxs.tolist(), atomidxs.tolist())])
 
         srcidxs = atomidxs
         dstidxs = ghostidxs % N
         cellidxs = ghostidxs // N
-        # print('\nsrcidxs: ', srcidxs)
-        # print('\ndstidxs: ', dstidxs)
-        # print('\ncellidxs: ', cellidxs)
-        # 4. EXPLICITLY DELETE SELF-LOOPS (Same atom AND Same Cell)
-        # If src == dst AND it's in the home cell (cell shift is [0,0,0])
+
         is_home_cell = (stack[cellidxs] == 0).all(dim=1)
         is_self_loop = (srcidxs == dstidxs) & is_home_cell
 
@@ -65,10 +51,8 @@ class CrystalGraphTensor:
         dstidxs = dstidxs[valid_edges]
         cellidxs = cellidxs[valid_edges]
 
-        # print('\nedges: ', edges.dtype, edges.shape)
-        # print('\nedges: ', [tuple(row.tolist()) for row in edges])
         shifts = stack[cellidxs]
-        # print('\nshifts: ', [tuple(row.tolist()) for row in shifts])
+        
         edges = torch.stack((srcidxs, dstidxs), axis=1)
 
         return edges.t(), shifts
